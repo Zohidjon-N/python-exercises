a1906c1627698222fc40e189640d0a82c860f051
Zohidjon-N <najmiddinovzohidjon2005@gmail.com>
2026-08-25 20:25:57 +0500

    intro class

--- OOP/intro_class.py
+++ OOP/intro_class.py
@@ -8,11 +8,13 @@ class User:
 
 user1= User('Zohidjon','nojdihoz','soosheyyov@gmail.com')
 print(user1.get_info())
+user2 = User('Saman','saman06@uz',"saman<3@vovo.com")
+print(user2.get_info())
 
 name = input("Your name>> ")
 username = input('Your username>> ')
 email = input('Your email>> ')
 
-user2= User(name,username,email)
+user3= User(name,username,email)
 
 print(user2.get_info())
