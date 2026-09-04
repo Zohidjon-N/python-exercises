c0c927c51ae71642548b5883bdb9ad47c6b0d92b
Zohidjon-N <najmiddinovzohidjon2005@gmail.com>
2026-09-04 07:20:16 +0500

    multi-lvl inheritance

---
+++ OOP/inheritance.py
@@ -0,0 +1,42 @@
+class Person:
+    def __init__(self, name,surname,y_birth):
+        self.name = name
+        self.surname = surname
+        self.y_birth = y_birth
+
+
+    def get_info(self):
+        return f"Name: {self.name}\nSurname: {self.surname}\nYear of birth: {self.y_birth}"  
+
+    def get_age(self,year):
+        return year - self.y_birth  
+
+
+
+class User(Person):
+    def __init__(self,name,surname,y_birth,email):
+        super().__init__(name,surname,y_birth)
+        self.email = email
+
+
+    def get_info(self):
+        return f"{super().get_info()}\nEmail: {self.email}"  #polymorphism
+
+
+user = User('Asad','Iqbolov',2005,'asad.iqbo77@gmail.com')
+print(user.get_info())
+
+
+class Admin(User): #Multi-level inheritance
+    def __init__(self,name,surname,y_birth,email):
+        super().__init__(name,surname,y_birth,email)
+        self.death_note = []
+
+    def ban_user(self, name):
+        self.death_note.append(name)
+        return f"Blocked: {name}"
+
+admin = Admin('adm','ADM',2000,'nomailcom')
+
+print(admin.ban_user('aziz'))
+        
